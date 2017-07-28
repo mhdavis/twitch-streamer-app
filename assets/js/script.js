6bb3f0788c6c1c836ed40f3a8c0c7563047fdbe2
Michael Davis <mhdavis1993@gmail.com>
2017-07-28 18:00:17 -0400

    refactored postStreamInfo to use str interpolation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ function getStreamInfo() {
                   'freecodecamp',
                   'ESL_SC2',
                   'imaqtpie',
-                  'Fate_Twisted_NA',
                   'brtt',
                   'MOONMOON_OW',
                   'A_Seagull',
@@ -37,30 +36,32 @@ function getStreamInfo() {
 
 /*-- If streamer streamer online, appends stream details to html page --*/
 function postStreamInfo(userUrl, userStatus, userLogo, displayName, streamTitle) {
+  let formattedCard =
+  `
+  <div id="${displayName.toLowerCase()}" class="users">
+    <div class="col s12 m6 l4">
+      <div class="card large hoverable">
+        <div class="user-logo card-image waves-effect waves-block waves-light">
+          <img class="activator" src="${userLogo}">
+        </div>
+        <div class="card-content">
+          <span class="display-name card-title activator grey-text text-darken-4">${displayName}<i class="vert-icon material-icons right">more_vert</i>
+          <p>
+            <div class="stream-button right-align">
+              <a class="stream-link waves-effect waves-light btn btn-large" href="${userUrl}" target="_blank">Stream</a>
+            </div>
+          </div>
+          </p>
+          <div class="card-reveal">
+            <span class="display-name card-title grey-text text-darken-4">${displayName}<i class="vert-icon material-icons right">more_vert</i></span>
+            <p id="stream-title">${streamTitle}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 
-  $('#' + userStatus).append(
-    '<div id="' + displayName.toLowerCase() + '" class="users">' +
-      '<div class="col s12 m6 l4">' +
-        '<div class="card large hoverable">' +
-          '<div class="user-logo card-image waves-effect waves-block waves-light">' +
-            '<img class="activator" src="' + userLogo +'">' +
-          '</div>' +
-          '<div class="card-content">' +
-            '<span class="display-name card-title activator grey-text text-darken-4">' + displayName + '<i class="vert-icon material-icons right">more_vert</i>' +
-          '<p>' +
-            '<div class="stream-button right-align">' +
-              '<a class="stream-link waves-effect waves-light btn btn-large" href="' + userUrl + '" target="_blank">Stream</a>' +
-            '</div>' +
-          '</div>' +
-          '</p>' +
-          '<div class="card-reveal">' +
-            '<span class="display-name card-title grey-text text-darken-4">' + displayName + '<i class="vert-icon material-icons right">more_vert</i></span>' +
-            '<p id="stream-title">' + streamTitle + '</p>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  );
+  $('#' + userStatus).append(formattedCard);
 }
 
 // All / Online /  Offline Filtering
